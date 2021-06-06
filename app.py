bc5cecc20aa3e087270b96be02ea7cb29d05f7ea
JeronimoC23 <jeronimoclinaz@gmail.com>
2021-06-05 21:25:17 -0300

    Agregando titulo y geometria

---
+++ app.py
@@ -0,0 +1,32 @@
+"""
+Programa:
+-ventana
+-tamaño fijo
+-no redimencionable
+-un menu
+-diferentes pantallas
+-formulario para añadir productos
+-guardar datos (temporalmente)
+-mostrar datos listados en la pantalla Home
+-opcion de salir
+"""
+
+from tkinter import *
+
+#Definicion de pantalla
+ventana = Tk()
+
+#Confiruaciones de pantalla
+ventana.geometry("500x500")
+ventana.title("Proyecto Tkinter")
+ventana.resizable(0, 0)
+
+
+
+
+
+
+
+
+#Funcion de la ventana
+ventana.mainloop()
